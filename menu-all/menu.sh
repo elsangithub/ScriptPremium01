f9a8042662fefb9b3cf4d247d16b31938cdf68a9
febrystores <89815036+febrystores@users.noreply.github.com>
2021-09-07 09:43:21 +0800

    Add files via upload

--- menu-all/menu.sh
+++ menu-all/menu.sh
@@ -29,7 +29,7 @@ DOMAIN=$(cat /etc/v2ray/domain)
 	echo -e "   \e[032;1mIPVPS:\e[0m $IPVPS"
 	echo -e "   \e[032;1mDOMAIN:\e[0m $DOMAIN"
 echo -e  ""
-echo -e  "   -------------------------MENU OPTIONS---------------------" | lolcat
+echo -e  "   -------------------------MENU OPTIONS------------------------" | lolcat
 echo -e   "   1\e[1;33m)\e[m SSH & OpenVPN Menu"
 echo -e   "   2\e[1;33m)\e[m Panel Wireguard "
 echo -e   "   3\e[1;33m)\e[m Panel L2TP & PPTP Account"
@@ -38,26 +38,33 @@ echo -e   "   5\e[1;33m)\e[m Panel SSR & SS Account"
 echo -e   "   6\e[1;33m)\e[m Panel V2Ray"
 echo -e   "   7\e[1;33m)\e[m Panel VLess"
 echo -e   "   8\e[1;33m)\e[m Panel TRojan"
-echo -e   "  \e[1;32m----------------------------------------------------------" | lolcat
-echo -e   "                             SYSTEM MENU" | lolcat 
-echo -e   "  \e[1;32m----------------------------------------------------------" | lolcat
+echo -e   "  \e[1;32m------------------------------------------------------------\e[m" | lolcat
+echo -e   "                             SYSTEM MENU\e[m" | lolcat 
+echo -e   "  \e[1;32m------------------------------------------------------------\e[m" | lolcat
 echo -e   "   9\e[1;33m)\e[m   Add Subdomain Host For VPS"
 echo -e   "   10\e[1;33m)\e[m  Renew Certificate V2RAY"
 echo -e   "   11\e[1;33m)\e[m  Change Port All Account"
-echo -e   "   12\e[1;33m)\e[m  Webmin Menu"
-echo -e   "   13\e[1;33m)\e[m  Limit Bandwith Speed Server"
-echo -e   "   14\e[1;33m)\e[m  Check Usage of VPS Ram" 
-echo -e   "   15\e[1;33m)\e[m  Reboot VPS"
-echo -e   "   16\e[1;33m)\e[m  Speedtest VPS"
-echo -e   "   17\e[1;33m)\e[m  Information Display System" 
-echo -e   "   18\e[1;33m)\e[m  Info Script Auto Install"
-echo -e   "   19\e[1;33m)\e[m  Install BBR"
-echo -e   "   20\e[1;33m)\e[m  Add ID Cloudflare"
-echo -e   "  \e[1;32m----------------------------------------------------------" | lolcat
+echo -e   "   12\e[1;33m)\e[m  Autobackup Data VPS"
+echo -e   "   13\e[1;33m)\e[m  Backup Data VPS"
+echo -e   "   14\e[1;33m)\e[m  Restore Data VPS"
+echo -e   "   15\e[1;33m)\e[m  Webmin Menu"
+echo -e   "   16\e[1;33m)\e[m  Limit Bandwith Speed Server"
+echo -e   "   17\e[1;33m)\e[m  Check Usage of VPS Ram" 
+echo -e   "   18\e[1;33m)\e[m  Reboot VPS"
+echo -e   "   19\e[1;33m)\e[m  Speedtest VPS"
+echo -e   "   20\e[1;33m)\e[m  Information Display System" 
+echo -e   "   21\e[1;33m)\e[m  Info Script Auto Install"
+echo -e   "   22\e[1;33m)\e[m  Install BBR"
+echo -e   "   23\e[1;33m)\e[m  Add ID Cloudflare"
+echo -e   "   24\e[1;33m)\e[m  Cloudflare Add-Ons"
+echo -e   "   25\e[1;33m)\e[m  Pointing BUG"
+echo -e   "   26\e[1;33m)\e[m  Clear log"
+echo -e   "   27\e[1;33m)\e[m  Auto Reboot"
+echo -e   "  \e[1;32m------------------------------------------------------------\e[m" | lolcat
 echo -e   "   x)   Exit" | lolcat
-echo -e   "  \e[1;32m----------------------------------------------------------" | lolcat
+echo -e   "  \e[1;32m------------------------------------------------------------\e[m" | lolcat
 echo -e   ""
-read -p "     Select From Options [1-20 or x] :  " menu
+read -p "     Select From Options [1-8 or x] :  " menu
 echo -e   ""
 case $menu in
 1)
@@ -94,30 +101,39 @@ certv2ray
 change-port
 ;;
 12)
-wbmn
+autobackup
 ;;
 13)
-limit-speed
+backup
 ;;
 14)
-ram
+restore
 ;;
 15)
-reboot
+wbmn
 ;;
 16)
-speedtest
+limit-speed
 ;;
 17)
-info
+ram
 ;;
 18)
-about
+reboot
 ;;
 19)
-bbr
+speedtest
 ;;
 20)
+info
+;;
+21)
+about
+;;
+22)
+bbr
+;;
+23)
 cff
 ;;
 24)
